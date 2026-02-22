47131bb8d72c57a36f895b9571cf81b24fc39a88
road2freedom77 <marcin.taranta@gmail.com>
2026-02-22 09:50:13 -0500

    Add files via upload

--- bridgetoretired/app/page.tsx
+++ bridgetoretired/app/page.tsx
@@ -1,18 +1,14 @@
-import { Hero }          from '@/components/Hero'
-import { HowItWorks }    from '@/components/HowItWorks'
-import { Calculator }    from '@/components/Calculator'
+import { Hero }           from '@/components/Hero'
+import { HowItWorks }     from '@/components/HowItWorks'
+import { Calculator }     from '@/components/Calculator'
 import { AffiliateTools } from '@/components/AffiliateTools'
-import { Newsletter }    from '@/components/Newsletter'
-import { BlogPreview }   from '@/components/BlogPreview'
-import { TopicPillars }  from '@/components/TopicPillars'
-import { allPosts }      from 'contentlayer/generated'
-import { compareDesc }   from 'date-fns'
+import { Newsletter }     from '@/components/Newsletter'
+import { BlogPreview }    from '@/components/BlogPreview'
+import { TopicPillars }   from '@/components/TopicPillars'
+import { getAllPosts }     from '@/lib/blog'
 
 export default function HomePage() {
-  const posts = allPosts
-    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-    .slice(0, 5)
-
+  const posts = getAllPosts().slice(0, 5)
   return (
     <>
       <Hero />
