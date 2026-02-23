364960de2947de4846f77a1dcfa4867ab7afab5f
road2freedom77 <marcin.taranta@gmail.com>
2026-02-23 18:55:05 -0500

    Update page.tsx

--- bridgetoretired/app/blog/[slug]/page.tsx
+++ bridgetoretired/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { allPosts } from '../../.contentlayer/generated'
+import { allPosts } from '@/.contentlayer/generated'
 import { notFound }           from 'next/navigation'
 import { format }             from 'date-fns'
 import { useMDXComponent }    from 'next-contentlayer2/hooks'
