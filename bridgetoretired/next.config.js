e64b400b53c53e3b2e300b3f8c960fbd5a9b446b
road2freedom77 <marcin.taranta@gmail.com>
2026-02-23 18:34:56 -0500

    Update next.config.js

--- bridgetoretired/next.config.js
+++ bridgetoretired/next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withContentlayer } = require('next-contentlayer2')
 
-module.exports = nextConfig
+const nextConfig = { reactStrictMode: true }
+
+module.exports = withContentlayer(nextConfig)
