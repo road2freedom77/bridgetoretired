83abf1c6d12faf5257f5e2907e7a014e83d540d9
road2freedom77 <marcin.taranta@gmail.com>
2026-02-25 21:04:40 -0500

    Update page.tsx

--- bridgetoretired/app/sign-up/[[...sign-up]]/page.tsx
+++ bridgetoretired/app/sign-up/[[...sign-up]]/page.tsx
@@ -12,7 +12,7 @@ export default function SignUpPage() {
         routing="path"
         path="/sign-up"
         signInUrl="/sign-in"
-        afterSignUpUrl="/pricing"
+        afterSignUpUrl="/auth-callback""
       />
     </div>
   )
