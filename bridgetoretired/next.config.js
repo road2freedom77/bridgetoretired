df011ad613dcb1f903f2539a4c904edb086521ad
road2freedom77 <marcin.taranta@gmail.com>
2026-03-09 16:14:15 -0400

    fix: redirect www to non-www for canonical host consistency  - Added permanent 301 redirect from www.bridgetoretired.com to bridgetoretired.com - Fixes mixed canonical signal in GSC hurting crawl equity

--- bridgetoretired/next.config.js
+++ bridgetoretired/next.config.js
@@ -3,6 +3,16 @@ const { withContentlayer } = require('next-contentlayer2')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.bridgetoretired.com' }],
+        destination: 'https://bridgetoretired.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = withContentlayer(nextConfig)
