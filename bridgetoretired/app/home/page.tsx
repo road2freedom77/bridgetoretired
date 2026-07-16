b29788a28a529fe6862975861e96ffa3f4d7980a
road2freedom77 <marcin.taranta@gmail.com>
2026-07-16 15:27:31 -0400

    Update page.tsx

--- bridgetoretired/app/home/page.tsx
+++ bridgetoretired/app/home/page.tsx
@@ -1,9 +1,9 @@
-import { auth } from '@clerk/nextjs/server'
+import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import FreeHome from '@/components/FreeHome'
 
 export default async function HomePage() {
-  const { userId } = (await auth())
-  if (!userId) redirect('/sign-in')
+  const user = await currentUser()
+  if (!user) redirect('/sign-in')
   return <FreeHome />
 }
