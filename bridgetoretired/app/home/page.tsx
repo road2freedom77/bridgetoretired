42fdd1962e58316dfe71f3376cf9a46d03f6cb65
road2freedom77 <marcin.taranta@gmail.com>
2026-07-16 15:39:38 -0400

    Update page.tsx

--- bridgetoretired/app/home/page.tsx
+++ bridgetoretired/app/home/page.tsx
@@ -1,24 +1,5 @@
-'use client'
+import FreeHome from '@/components/FreeHome'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { useUser } from '@clerk/nextjs'
-
-export default function AuthCallbackPage() {
-  const { user, isLoaded } = useUser()
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!isLoaded) return
-    if (!user) return
-
-    const isPro = user.publicMetadata?.isPro === true
-    if (isPro) {
-      router.replace('/pro-welcome')
-    } else {
-      router.replace('/home')
-    }
-  }, [user, isLoaded, router])
-
-  return null
+export default function HomePage() {
+  return <FreeHome />
 }
